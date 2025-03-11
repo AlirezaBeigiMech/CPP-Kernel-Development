8ec3a85f69bfc9fdb4af5e3721610364d6f41ac7
alirezabeigi <alirezabeigimech@gmail.com>
2025-03-10 23:36:59 -0700

    first commit

---
+++ kern_lib.h
@@ -0,0 +1,20 @@
+#ifndef KERN_LIB_H
+#define KERN_LIB_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void kmemset(void *dst, int c, unsigned int len);
+void *kcmemcpy(void *dst, void *src, unsigned int len);
+void *kcmemmove(void *dst, void *src, unsigned int len);
+int kcmemcmp(void *p1, void *p2, unsigned int len);
+void *kcmalloc(unsigned int size);
+void *kcrealloc(void *mem, unsigned int size);
+void kcfree(void *mem);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
